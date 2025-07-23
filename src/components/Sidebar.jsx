cf17187adf733a16ed9513c39683c152b462329e
Aliya <aliyahassan032@gmail.com>
2025-07-23 22:32:34 +0530

    track your adventures

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Sidebar.module.css";
+import AppNav from "./AppNav";
+import { Link, Outlet } from "react-router-dom";
+import Logo from "./Logo";
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <AppNav />
+
+      <Outlet />
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
